b6c99caf25befe661e0fbfa759d5fc3f3ce58e36
Sean Esteva <seangeleno@gmail.com>
2016-04-28 06:59:11 -0700

    added mouseout event listener, yellow square gets wider and smaller

--- logic.js
+++ logic.js
@@ -8,21 +8,13 @@
     startButton.style.marginLeft = "50%";
     startButton.style.marginTop = "35%";
     document.body.appendChild(startButton);
-    startButton.innerHTML = "Start Game"
+    startButton.innerHTML = "Start Game";
 
     startButton.addEventListener('click', function(){
 
       document.body.removeChild(startButton);
       // startButton.style.display = "none";
 
-      (function newStyle () {
-         var headID = document.getElementsByTagName('head')[0];
-         var newScript = document.createElement('link');
-         newScript.href = 'newStyle.css';
-         newScript.type = 'text/css';
-         newScript.rel = 'stylesheet';
-         headID.appendChild(newScript);
-      }());
 
       let container = document.createElement('div');
 
@@ -30,8 +22,6 @@
       container.style.width = "300px";
       container.style.position = "relative";
       container.style.left = "45%";
-      container.style.transform = "rotate (360deg)";
-      container.style.transition = "all 2s";
 
       document.body.appendChild(container);
 
@@ -43,18 +33,30 @@
       firstSquare.style.height = "150px";
       firstSquare.style.width = "150px";
       firstSquare.style.backgroundColor = "blue";
-      firstSquare.style.left = "25%";
-      firstSquare.style.top = "10%";
+
       firstSquare.style.position = "absolute";
       firstSquare.style.display = "inline-block";
+      firstSquare.className = ('firstSquare');
+
 
 
       firstSquare.addEventListener('click', function(){
 
-        playerArray.push("4");
+        playerArray.push("1");
         console.info(playerArray);
 
-      })
+        //CSS injection
+        // var newStyle = function () {
+        //    const headID = document.getElementsByTagName('head')[0];
+        //    const newScript = document.createElement('link');
+        //    newScript.href = 'newStyle.css';
+        //    newScript.type = 'text/css';
+        //    newScript.rel = 'stylesheet';
+        //    headID.appendChild(newScript);
+        // }();
+
+
+      });
 
       secondSquare.style.height = "150px";
       secondSquare.style.width = "150px";
@@ -65,11 +67,27 @@
       secondSquare.style.display = "inline-block";
       // secondSquare.style.left = "150px";
       secondSquare.style.top = "150px";
+      secondSquare.className = 'secondSquare';
       secondSquare.addEventListener('click', function(){
 
         playerArray.push("3");
         console.info(playerArray);
 
+      });
+
+      fourthSquare.addEventListener('mouseover', function(){
+        console.warn("I'm hovering on yellow square");
+
+        fourthSquare.style.width = "350px";
+        fourthSquare.style.transition = "width 2s";
+
+      });
+
+      fourthSquare.addEventListener('mouseout', function(){
+        console.info("I'm leaving the yellow square");
+
+        fourthSquare.style.width = "150px";
+        fourthSquare.style.transition = "width 2s";
       })
 
       thirdSquare.style.height = "150px";
@@ -80,6 +98,7 @@
       thirdSquare.style.position = "absolute";
       thirdSquare.style.display = "inline-block";
       thirdSquare.style.left = "150px";
+      thirdSquare.className = "thirdSquare";
       thirdSquare.addEventListener('click', function(){
 
         playerArray.push("2");
@@ -94,11 +113,12 @@
       // fourthSquare.style.top = "27%";
       fourthSquare.style.display = "inline-block";
       fourthSquare.style.position = "absolute";
-      firstSquare.style.left = "150px";
-      firstSquare.style.top = "150px";
+      fourthSquare.style.left = "150px";
+      fourthSquare.style.top = "150px";
+      fourthSquare.className = ('fourthSquare');
       fourthSquare.addEventListener('click', function(){
 
-        playerArray.push("1");
+        playerArray.push("4");
         console.info(playerArray);
 
       })
